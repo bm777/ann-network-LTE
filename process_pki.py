c744b26d508927e03cd0b3df2600e4f7bc665980
Bayangmbe Mounmo <bayangp0@gmail.com>
2020-12-04 03:23:49 +0100

    Added python funtion to generate

--- process_pki.py
+++ process_pki.py
@@ -25,7 +25,7 @@ def read_pgw(filename, town):
             if sheet.cell_value(i, 0) == "Start Time":
                 st = True
             else:
-                if town in sheet.cell_value(i, 2):
+                if town == sheet.cell_value(i, 2):
                     print( sheet.cell_value(i, 0), sheet.cell_value(i, 2), sheet.cell_value(i, 5), sheet.cell_value(i, 7), sheet.cell_value(i, 8))
                     rows.append([sheet.cell_value(i, 0), sheet.cell_value(i, 2), sheet.cell_value(i, 5), sheet.cell_value(i, 7), sheet.cell_value(i, 8)])
                     # severities.append(sheet.cell_value(i, 1))
@@ -34,6 +34,23 @@ def read_pgw(filename, town):
     df = pd.DataFrame(rows)
     df.to_csv("dataset.csv", index=False, header=column_names)
     return
+
+def get_town(filename):
+    wb = xlrd.open_workbook(filename)
+    sheet = wb.sheet_by_index(0)
+    rows = []
+
+    for i in range(8,sheet.nrows):
+        if sheet.cell_value(i, 0) == "Start Time" or sheet.cell_value(i, 0) != "":
+            if sheet.cell_value(i, 0) == "Start Time":
+                st = True
+            else:
+                rows.append(sheet.cell_value(i, 2))
+    final = []
+    for e in rows:
+        if e not in final:
+            final.append(e)
+    return final
 
 def read_ugw(filename, town):
     wb = xlrd.open_workbook(filename)
@@ -45,7 +62,7 @@ def read_ugw(filename, town):
             if sheet.cell_value(i, 0) == "Start Time":
                 st = True
             else:
-                if town in sheet.cell_value(i, 2):
+                if town == sheet.cell_value(i, 2):
                     print(i, sheet.cell_value(i, 4), sheet.cell_value(i, 5))
                     rows.append([sheet.cell_value(i, 4), sheet.cell_value(i, 5)])
                 # severities.append(sheet.cell_value(i, 1))
@@ -63,6 +80,11 @@ def concat(ds1, ds2):
     df.to_csv("final_ds.csv", index=False)
 
 if __name__ == '__main__':
-    # read_pgw("s_pgw.xlsx", "YDE")
-    # read_ugw("ugw.xlsx", "YDE")
-    concat("dataset.csv", "dataset2.csv")
+    filename = "ugw"
+    print("result : ",get_town("ugw.xlsx"))
+    # if "ugw" in filename:
+    #     read_ugw("ugw.xlsx", "YDE")
+    # else:
+    #     read_pgw("s_pgw.xlsx", "YDE")
+
+    # concat("dataset.csv", "dataset2.csv")
